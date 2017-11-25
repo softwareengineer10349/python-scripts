710b90ee015c1a3bc405d6870e293a1ccfbe8dca
Tina <softwareengineer10349@gmail.com>
2017-11-25 22:24:27 +1000

    Drammatically changed table structure to simplify things

--- writeJobStatsToDB.py
+++ writeJobStatsToDB.py
@@ -12,29 +12,37 @@ general_folder_for_output = "Files_generated_by_script"
 username_password_file = open("username password file.txt", "r")
 for line in username_password_file:
     username_password_combined = line.split(",")
-    username = line[0]
-    password = line[1]
+    username = username_password_combined[0]
+    password = username_password_combined[1].strip()
 db = MySQLdb.connect("localhost",username,password,"Jobs" )
 
 cursor = db.cursor()
 
+sql_to_create_master = "CREATE TABLE IF NOT EXISTS `master_table` (job_skill VARCHAR(255), PRIMARY KEY(job_skill))"
+cursor.execute(sql_to_create_master)
+
+sql_to_create_job_table = "CREATE TABLE IF NOT EXISTS `job_table` (url VARCHAR(255), city VARCHAR(255), job_skill VARCHAR(255), PRIMARY KEY(url,city,job_skill))"
+cursor.execute(sql_to_create_job_table)
+
 for each_files in glob.glob(general_folder_for_output + "/*" ):
     if ("lobal" not in each_files and "Run" not in each_files and "Error" not in each_files):
         file_string = each_files.replace("/all_urls_matching_keyword_","").replace(".txt","").replace(general_folder_for_output, "").replace("(","").replace(")","").replace("^"," not ").replace("#"," sharp").replace(" ","_").replace("-","_").strip()
-        print "File string: " + file_string + "\n"
         array_to_split_city = file_string.split("_",1)
         city_of_job = array_to_split_city[0]
         name_of_skill = array_to_split_city[1]
-        sql = """CREATE TABLE IF NOT EXISTS""" + "`" + name_of_skill + """` (url VARCHAR(255), city VARCHAR(255), PRIMARY KEY(url,city))"""
-        sql = sql.strip()
-        cursor.execute(sql)
+        try:
+            sql = "INSERT INTO `master_table` (`job_skill`) VALUES (" + "'" + name_of_skill + "')"
+            cursor.execute(sql)
+            print "Ran SQL:" sql + "\n"
+        except:
+            print "Did not run SQL: " + sql + "\n"
         file_to_read = open(each_files, "r")
         for each_line in file_to_read:
             try:
-                sql = """INSERT INTO """ + "`" + name_of_skill + """` (url,city) VALUES('""" + each_line + """','""" + city_of_job +  """')"""
+                sql = """INSERT INTO """ + "`job_table` (`url`,`city`,`job_skill`) VALUES('""" + each_line.strip() + """','""" + city_of_job +  """','""" + name_of_skill + """')"""
                 cursor.execute(sql)
-                #print "Sql ran: "+sql + "\n"
+                print "Ran SQL:" sql+ "\n"
             except:
                 print "Did not run SQL: " + sql + "\n"
-
+db.commit()
 db.close()
