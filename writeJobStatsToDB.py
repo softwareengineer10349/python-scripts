575d9c1b65e37ba4076a7f01410f2216f2e24fe0
Tina <softwareengineer10349@gmail.com>
2017-12-03 20:16:24 +1000

    Added field in DB to allow search of type of skill

--- writeJobStatsToDB.py
+++ writeJobStatsToDB.py
@@ -18,7 +18,7 @@ db = MySQLdb.connect("localhost",username,password,"Jobs" )
 
 cursor = db.cursor()
 
-sql_to_create_master = "CREATE TABLE IF NOT EXISTS `master_table` (job_skill VARCHAR(255), PRIMARY KEY(job_skill))"
+sql_to_create_master = "CREATE TABLE IF NOT EXISTS `master_table` (job_skill VARCHAR(255), type_of_skill VARCHAR(255), PRIMARY KEY(job_skill,type_of_skill))"
 cursor.execute(sql_to_create_master)
 
 sql_to_create_job_table = "CREATE TABLE IF NOT EXISTS `job_table` (url VARCHAR(255), city VARCHAR(255), job_skill VARCHAR(255), PRIMARY KEY(url,city,job_skill))"
@@ -26,22 +26,25 @@ cursor.execute(sql_to_create_job_table)
 
 for each_files in glob.glob(general_folder_for_output + "/*" ):
     if ("lobal" not in each_files and "Run" not in each_files and "Error" not in each_files):
-        file_string = each_files.replace("/all_urls_matching_keyword_","").replace(".txt","").replace(general_folder_for_output, "").replace("(","").replace(")","").replace("^"," not ").replace("#"," sharp").replace(" ","_").replace("-","_").strip()
-        array_to_split_city = file_string.split("_",1)
+        file_string = each_files.replace("/all_urls_matching_keyword_","").replace(".txt","").replace(general_folder_for_output, "").strip()#.replace("(","").replace(")","").replace("^"," not ").replace("#"," Sharp").replace(" ","_").replace("-","_").strip()
+        array_to_split_city = file_string.split("_",2)
         city_of_job = array_to_split_city[0]
         name_of_skill = array_to_split_city[1]
-        try:
-            sql = "INSERT INTO `master_table` (`job_skill`) VALUES (" + "'" + name_of_skill + "')"
-            cursor.execute(sql)
-            print "Ran SQL:" sql + "\n"
-        except:
-            print "Did not run SQL: " + sql + "\n"
+        all_types_of_skills = array_to_split_city[2]
+        types_of_skills_array = all_types_of_skills.split("_")
+        for each_type in types_of_skills_array:
+            try:
+                sql = "INSERT INTO `master_table` (`job_skill`, `type_of_skill`) VALUES (" + "'" + name_of_skill + "','" + each_type + "')"
+                cursor.execute(sql)
+                print "Ran SQL:" + sql + "\n"
+            except:
+                print "Did not run SQL: " + sql + "\n"
         file_to_read = open(each_files, "r")
         for each_line in file_to_read:
             try:
                 sql = """INSERT INTO """ + "`job_table` (`url`,`city`,`job_skill`) VALUES('""" + each_line.strip() + """','""" + city_of_job +  """','""" + name_of_skill + """')"""
                 cursor.execute(sql)
-                print "Ran SQL:" sql+ "\n"
+                print "Ran SQL:" + sql+ "\n"
             except:
                 print "Did not run SQL: " + sql + "\n"
 db.commit()
