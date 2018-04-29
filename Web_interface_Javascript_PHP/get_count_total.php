df46d5b777b17dbb23146d51894895729e956ace
Tina <softwareengineer10349@gmail.com>
2018-04-29 15:53:46 +1000

    Seperated out DB connection

--- Web_interface_Javascript_PHP/get_count_total.php
+++ Web_interface_Javascript_PHP/get_count_total.php
@@ -1,31 +1,12 @@
 <?php
-$servername = "localhost";
-$dbname = "Jobs";
-$username = "";
-$password = "";
-
-$file_handle = fopen("username password file.txt", "r");
-while (!feof($file_handle)) {
-   $line = trim(fgets($file_handle));
-   $uname_pword = explode(",",$line);
-   $username = $uname_pword[0];
-   $password = $uname_pword[1];
-   break;
-}
-fclose($file_handle);
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'db_connection_start.php';
 
 $count_total = 0;
 
 $firsttable = mysqli_real_escape_string($conn,$_GET["table_name"]);
 $city = mysqli_real_escape_string($conn,$_GET["city_name"]);
 
-$sql = "CALL sp_get_count_of_all_jobs_in_city('" . $firsttable . "', '" . $city . "')"; 
+$sql = "CALL sp_get_count_of_all_jobs_in_city('" . $firsttable . "', '" . $city . "')";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
--- Web_interface_Javascript_PHP/get_job_numbers.php
+++ Web_interface_Javascript_PHP/get_job_numbers.php
@@ -1,24 +1,5 @@
 <?php
-$servername = "localhost";
-$dbname = "Jobs";
-$username = "";
-$password = "";
-
-$file_handle = fopen("username password file.txt", "r");
-while (!feof($file_handle)) {
-   $line = trim(fgets($file_handle));
-   $uname_pword = explode(",",$line);
-   $username = $uname_pword[0];
-   $password = $uname_pword[1];
-   break;
-}
-fclose($file_handle);
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'db_connection_start.php';
 
 $all_matches = [];
 
