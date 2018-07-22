99f4bbae79b233d7f49751eb807b9044542e924f
Tina <softwareengineer10349@gmail.com>
2018-07-22 10:49:14 +1000

    Added POST as well as GET

--- Security_Scripts/web_injection_checker.py
+++ Security_Scripts/web_injection_checker.py
@@ -16,14 +16,24 @@ def resultOfSendRequest(s, full_url, cookies, values):
     req = s.post(full_url, values, cookies=cookies)
     return req.text
 
+testsPassed = 0
+testsFailed = 0
+
 def testResult(textIfVulnerable, printSuccess, printFail, full_body,myUniqueText):
+    global testsFailed
+    global testsPassed
     if textIfVulnerable in full_body:
         print(printFail)
+        testsFailed+=1
     else:
         print(printSuccess)
+        testsPassed+=1
     for line in full_body.splitlines():
         if myUniqueText in line:
             print "For the above result, the line with my text was rendered as: " + line
+
+def printThisHeading(heading):
+    print("\n=========== Running tests for: " + heading +" ===========")
 
 
 settings = initialiseSettings()
@@ -32,7 +42,7 @@ username = settings["username"]
 password = settings["password"]
 form_val = "Y"
 values = { 'login': username,'password': password, 'form': form_val}
-cookies = dict(security_level='2')
+cookies = dict(security_level='1')
 
 #need to login before testing real URL
 resultOfSendRequest(s,"http://localhost/bWAPP/login.php",cookies,values)
@@ -41,11 +51,29 @@ base_url = "http://localhost/bWAPP/htmli_get.php?firstname=A&lastname="
 attack_url = "<h2>TTTEST<h2>"
 end_of_url = "&form=submit"
 
-#test real URL
+printThisHeading("GET HTML injection tests")
+#test GET URL
 full_url = base_url + attack_url + end_of_url
 web_result = resultOfSendRequest(s,full_url,cookies,values)
 testResult("<h2>TTTEST", "SAFE FROM BASIC HTML TAG INJECTION", "VULNERABLE TO BASIC HTML TAG INJECTION", web_result, "TTTEST")
 
 full_url = base_url + urllib.quote_plus(urllib.quote_plus(attack_url)) + end_of_url
 web_result = resultOfSendRequest(s,full_url,cookies,values)
-testResult("<h2>TTTEST", "SAFE FROM ENCODED HTML TAG INJECTION", "VULNERABLE TO ENCODED HTML TAG INJECTION", web_result, "TTTEST")
+testResult("<h2>TTTEST", "SAFE FROM ENCODED HTML TAG INJECTION", "VULNERABLE TO ENCODED HTML TAG INJECTION, YOU SHOULD USE FUNCTION htmlspecialchars", web_result, "TTTEST")
+
+printThisHeading("POST HTML injection tests")
+#test POST url
+base_url = "http://localhost/bWAPP/htmli_post.php"
+values = { 'form': form_val, 'firstname' : 'A', 'lastname' : attack_url}
+full_url = base_url
+web_result = resultOfSendRequest(s,full_url,cookies,values)
+testResult("<h2>TTTEST", "SAFE FROM BASIC HTML TAG INJECTION", "VULNERABLE TO BASIC HTML TAG INJECTION", web_result, "TTTEST")
+
+values = { 'form': form_val, 'firstname' : 'A', 'lastname' : urllib.quote_plus(attack_url)}
+full_url = base_url
+web_result = resultOfSendRequest(s,full_url,cookies,values)
+testResult("<h2>TTTEST", "SAFE FROM ENCODED HTML TAG INJECTION", "VULNERABLE TO ENCODED HTML TAG INJECTION, YOU SHOULD USE FUNCTION htmlspecialchars", web_result, "TTTEST")
+
+print("\n================ Totals =================")
+print("Total tests passed: "+str(testsPassed))
+print("Total test failed: "+str(testsFailed))
